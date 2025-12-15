ab5e5cd9bf655335acc578762076a8ab589767b8
Meshil Maring <dsmeshilmaring13@gmail.com>
2025-12-15 19:05:02 +0530

    design: Create account design

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
-import Home from "./components/pages/Home";
 import { Route, Routes } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Signup from "./components/pages/Signup";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
+      <Route path="/sign-up" element={<Signup />}></Route>
     </Routes>
   );
 }
--- client/src/components/pages/Signup.tsx
+++ client/src/components/pages/Signup.tsx
@@ -1,24 +1,65 @@
+import { Link } from "react-router-dom";
+
 const Signup = () => {
   return (
-    <form>
-      <label>User Name</label>
-      <input type="text" name="name" placeholder="Enter your name"></input>
+    <section className="flex justify-center items-center h-screen">
+      <form className="flex flex-col p-8 bg-white w-96 text-black rounded-2xl gap-4 shadow-2xl shadow-white/20">
+        <div className="flex flex-col gap-2">
+          <label>User Name</label>
+          <input
+            className="border-2 rounded-sm border-black/40 px-2 py-1"
+            type="text"
+            name="name"
+            placeholder="Enter your name"
+          ></input>
+        </div>
 
-      <label>Email</label>
-      <input type="email" name="email" placeholder="Enter your email"></input>
+        <div className="flex flex-col gap-2">
+          <label>Email</label>
+          <input
+            className="border-2 rounded-sm border-black/40 px-2 py-1"
+            type="email"
+            name="email"
+            placeholder="Enter your email"
+          ></input>
+        </div>
 
-      <label>Password</label>
-      <input
-        type="text"
-        name="password"
-        placeholder="Enter your password"
-      ></input>
+        <div className="flex flex-col gap-2">
+          <label>Password</label>
+          <input
+            className="border-2 rounded-sm border-black/40 px-2 py-1"
+            type="text"
+            name="password"
+            placeholder="Enter your password"
+          ></input>
+        </div>
 
-      <label>Confirm Password</label>
-      <input type="text" name="confirm-password"></input>
+        <div className="flex flex-col gap-2">
+          <label>Confirm Password</label>
+          <input
+            className="border-2 rounded-sm border-black/40 px-2 py-1"
+            type="text"
+            name="confirm-password"
+            placeholder="Renter your password"
+          ></input>
+        </div>
 
-      <button type="submit">Submit</button>
-    </form>
+        <button
+          className="bg-blue-700 text-white p-2 rounded-sm cursor-pointer mt-8"
+          type="submit"
+        >
+          Create account
+        </button>
+
+        <label className="text-center">
+          Already have an account ?{" "}
+          <Link className="text-blue-700 underline" to={"/log-in"}>
+            Log in
+          </Link>{" "}
+          here
+        </label>
+      </form>
+    </section>
   );
 };
 
