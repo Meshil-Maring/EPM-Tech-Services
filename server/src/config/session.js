d048d7a6b6367997b4348e58929d44b2c3e70a2f
Meshil Maring <dsmeshilmaring13@gmail.com>
2025-12-20 10:47:53 +0530

    fix: signup and login bugs

--- server/src/config/session.js
+++ server/src/config/session.js
@@ -10,7 +10,7 @@ export default {
   cookie: {
     httpOnly: true,
     secure: false,
-    // sameSite: "lax",
-    maxAge: 1000 * 60 * 60,
+    sameSite: "lax",
+    maxAge: 1000 * 60 * 60 * 24 * 30,
   },
 };
