6ac36934d8eb5cf0a82030ed0c04e490f468a991
Meshil Maring <dsmeshilmaring13@gmail.com>
2025-12-26 22:41:00 +0530

    optimize the code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,54 +1,68 @@
 import { Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
 
-import Home from "./pages/Home";
-import Signup from "./pages/Signup";
-import Login from "./pages/Login";
-import Transaction from "./pages/Transaction";
 import ProtectedRoute from "./routes/ProtectedRoute";
 
-import AboutUs from "./pages/footer/About-us";
-// import Careers from "./pages/footer/Careers";
-import ServiceFAQs from "./pages/footer/ServiceFAQ";
-import Web from "./pages/footer/Web";
-import DevelopmentServices from "./pages/footer/DevelopmentServices";
-import Design from "./pages/footer/DesignServices";
-import Consulting from "./pages/footer/Consulting";
-import ResourceDocumentation from "./pages/footer/Documentation";
-import Support from "./pages/footer/Support";
-import TermsAndService from "./pages/footer/TermsService";
-import CookiePolicy from "./pages/footer/CookiePolicy";
-import PrivacyPolicy from "./pages/footer/PrivacyPolicy";
+// Lazy pages
+const Home = lazy(() => import("./pages/Home"));
+const Signup = lazy(() => import("./pages/Signup"));
+const Login = lazy(() => import("./pages/Login"));
+const Transaction = lazy(() => import("./pages/Transaction"));
+
+// Footer pages
+const AboutUs = lazy(() => import("./pages/footer/About-us"));
+const ServiceFAQs = lazy(() => import("./pages/footer/ServiceFAQ"));
+const Web = lazy(() => import("./pages/footer/Web"));
+const DevelopmentServices = lazy(
+  () => import("./pages/footer/DevelopmentServices")
+);
+const Design = lazy(() => import("./pages/footer/DesignServices"));
+const Consulting = lazy(() => import("./pages/footer/Consulting"));
+const ResourceDocumentation = lazy(
+  () => import("./pages/footer/Documentation")
+);
+const Support = lazy(() => import("./pages/footer/Support"));
+const TermsAndService = lazy(() => import("./pages/footer/TermsService"));
+const CookiePolicy = lazy(() => import("./pages/footer/CookiePolicy"));
+const PrivacyPolicy = lazy(() => import("./pages/footer/PrivacyPolicy"));
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/auth/sign-up" element={<Signup />}></Route>
-      <Route path="/auth/log-in" element={<Login />}></Route>
+    <Suspense
+      fallback={
+        <div className="h-screen flex items-center justify-center text-white">
+          Loading...
+        </div>
+      }
+    >
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth/sign-up" element={<Signup />} />
+        <Route path="/auth/log-in" element={<Login />} />
 
-      <Route
-        path="/transaction"
-        element={
-          <ProtectedRoute>
-            <Transaction />
-          </ProtectedRoute>
-        }
-      ></Route>
+        <Route
+          path="/transaction"
+          element={
+            <ProtectedRoute>
+              <Transaction />
+            </ProtectedRoute>
+          }
+        />
 
-      {/* Footer routes */}
-      <Route path="/about-us" element={<AboutUs />}></Route>
-      {/* <Route path="/careers" element={<Careers />}></Route> */}
-      <Route path="/service-faqs" element={<ServiceFAQs />}></Route>
-      <Route path="/web" element={<Web />}></Route>
-      <Route path="/development" element={<DevelopmentServices />}></Route>
-      <Route path="/ui/ux-design" element={<Design />}></Route>
-      <Route path="/consulting" element={<Consulting />}></Route>
-      <Route path="/documentation" element={<ResourceDocumentation />}></Route>
-      <Route path="/support" element={<Support />}></Route>
-      <Route path="/terms-and-service" element={<TermsAndService />}></Route>
-      <Route path="/cookie-policy" element={<CookiePolicy />}></Route>
-      <Route path="/privacy-policy" element={<PrivacyPolicy />}></Route>
-    </Routes>
+        {/* Footer routes */}
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/service-faqs" element={<ServiceFAQs />} />
+        <Route path="/web" element={<Web />} />
+        <Route path="/development" element={<DevelopmentServices />} />
+        <Route path="/ui/ux-design" element={<Design />} />
+        <Route path="/consulting" element={<Consulting />} />
+        <Route path="/documentation" element={<ResourceDocumentation />} />
+        <Route path="/support" element={<Support />} />
+        <Route path="/terms-and-service" element={<TermsAndService />} />
+        <Route path="/cookie-policy" element={<CookiePolicy />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+      </Routes>
+    </Suspense>
   );
 }
 
