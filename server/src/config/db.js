5b2a1f7708a000aa89f405b280d452dac4b52d04
Meshil Maring <dsmeshilmaring13@gmail.com>
2025-12-27 19:01:42 +0530

    fix database connection for production ready

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,10 +1,15 @@
-import { Pool } from "pg";
+import pkg from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
 
+const { Pool } = pkg;
+
 const db = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default db;
