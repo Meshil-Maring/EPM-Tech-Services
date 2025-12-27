8a5b9b16303e22b530203d43754f5bb1096edb31
Meshil Maring <dsmeshilmaring13@gmail.com>
2025-12-27 22:17:25 +0530

    change: superbase change into render database

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -7,9 +7,6 @@ const db = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
   family: 4,
-  max: 5,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 5000,
 });
 
 export default db;
